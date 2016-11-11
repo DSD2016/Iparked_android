ffae06adfba7ed624c75560f69fa48b0fc9b1032
Hrvoje <hz46826@fer.hr>
2016-11-11 10:55:48 +0100

    Added Beacon class

---
+++ app/src/main/java/com/dsd2016/iparked_android/MyClasses/Beacon.java
@@ -0,0 +1,35 @@
+package com.dsd2016.iparked_android.MyClasses;
+
+
+public class Beacon {
+
+    private short major;
+    private short minor;
+    private String uuid;
+
+    public short getMajor() {
+        return major;
+    }
+
+    public void setMajor(short major) {
+        this.major = major;
+    }
+
+    public short getMinor() {
+        return minor;
+    }
+
+    public void setMinor(short minor) {
+        this.minor = minor;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+
+}
