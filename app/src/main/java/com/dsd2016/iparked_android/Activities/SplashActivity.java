c53e9506fb604673f306b747e1ef18906aff70e7
saeedek <ektesabi.saeed@gmail.com>
2016-11-12 02:09:48 +0100

    Added Google Map,Custom Marker,User location,no triggers

--- app/src/main/java/com/dsd2016/iparked_android/Activities/SplashActivity.java
+++ app/src/main/java/com/dsd2016/iparked_android/Activities/SplashActivity.java
@@ -24,6 +24,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
             }
         }, 5000);
     }
